f53dc2b07033c8d5bde062dca5ed1003e0d1b9ae
Paul <212274079+FukkuraDev@users.noreply.github.com>
2025-06-20 02:10:01 +0100

    Fixed form issue with dangerouslySetInnerHTML

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -11,8 +11,8 @@ export default async function Page({params}: {params: Promise<{slug: string}>}) 
     return (
         <div>
             <h1 className="text-2xl font-bold border-b py-2 mb-5">{project.title}</h1>
-            <div className="mb-5" dangerouslySetInnerHTML={{__html: project.description}}></div>
-            <div dangerouslySetInnerHTML={{__html: project.content}}></div>
+            <div className="mb-5" dangerouslySetInnerHTML={{__html: project.description || ' ' }}></div>
+            <div dangerouslySetInnerHTML={{__html: project.content || ' ' }}></div>
         </div>
     );
 }
