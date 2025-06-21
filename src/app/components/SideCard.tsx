a6e84ab54f784f1cd2b6c74536f352f60d9adaa4
Paul <212274079+FukkuraDev@users.noreply.github.com>
2025-06-21 01:49:17 +0100

    fix: name issue with json, and styling

--- src/app/components/SideCard.tsx
+++ src/app/components/SideCard.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface SideCardProps {
     data: {
         [key: string]: any;
@@ -18,31 +20,42 @@ const SideCard = ({ data }: SideCardProps) => {
                 <p className="italic text-sm pt-5">Current known logo</p>
             </div>
 
-            <div className="border-b pb-1">
-                <div className="p-2 flex justify-between">
-                    <h2 className="font-bold">Announced</h2>
-                    <h2>{data.announcedDate || 'TBA'}</h2>
+            <div className="pt-1 pb-1 text-xl border-b font-bold">
+                Dates
+                <div className="font-normal text-base">
+                    <div className="p-2 flex justify-between">
+                        <h2 className="font-bold">Announced</h2>
+                        <h2>{data.announceddate || 'TBA'}</h2>
+                    </div>
+                    <div className="p-2 flex justify-between">
+                        <h2 className="font-bold">Release</h2>
+                        <h2>{data.releasedate || 'TBA'}</h2>
+                    </div>
                 </div>
-                <div className="p-2 flex justify-between">
-                    <h2 className="font-bold">Release</h2>
-                    <h2>{data.releaseDate || 'TBA'}</h2>
+            </div>
+            <div className="pt-1 pb-1 text-xl border-b font-bold">
+                Type
+                <div className="font-normal text-base">
+                    {data.projecttype}
                 </div>
             </div>
             <div className="pt-1 pb-1 text-xl border-b font-bold">
                 Links
                 <div className="font-normal text-base">
+
                     {data.links.length > 0 ? (
                         data.links.map((link: any, index: number) => (
-                            <div key={index}>
-                                <a href={link.url} target="_blank" rel="noopener noreferrer">{link.label}</a>
+                            <div key={index} >
+                                <Link href={link.url || ' '} target="_blank" className="text-violet-400">{link.label}</Link>
+
                             </div>
-                        ))
+                        )) //This is suboptimal, please help fix it
                     ) : (
-                        <p>No links available.</p> // Optional fallback UI if no links
+                        <p>No links available.</p>
                     )}
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
