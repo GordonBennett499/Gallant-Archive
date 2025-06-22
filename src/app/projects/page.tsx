dacd9ad7c2c78839d129fcb4d43c751ce082c11b
GordonBennett499 <gordonbennett1841@gmail.com>
2025-06-22 13:14:19 +0100

    fix: update text on projects archive

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ export default function ProjectsPage() {
     return (
         <div>
             <h1 className="text-2xl font-bold border-b py-2 mb-5">Projects</h1>
-            <p className="mb-5">Explore our projects below.</p>
+            <p className="mb-5">Below are all of the projects Paul is "working on".</p>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-5">
                 {Object.keys(projects).map((pid) => {
                     const project = projects[pid as keyof typeof projects];
