f2db60451e8eef6ce404570e191a97e8006f999d
GordonBennett499 <gordonbennett1841@gmail.com>
2025-06-23 16:27:55 +0100

    feat: add link to about in nav bar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,18 +5,23 @@ import { useState } from "react";
 
 const NavBar = () => {
 
-    const [buttonText, setButtonText] = useState("Has Paul got a job?");
+    const [buttonText, setButtonText] = useState("Does Paul have a job?");
     const checkJob = () => {
         setButtonText("No.");
         setTimeout(() => {
-            setButtonText("Has Paul got a job?");
+            setButtonText("Does Paul have a job?");
         }, 2000);
     }
 
     return (
         <header className="relative mx-auto w-full max-w-6xl px-10 py-5 flex flex-col md:flex-row gap-3 md:justify-between md:items-center">
             <Link href="/" className="text-3xl font-bold tracking-wide">Gallant Archive</Link>
-            <button className="bg-violet-400 text-white px-4 py-2 rounded-md cursor-pointer" onClick={checkJob}>{buttonText}</button>
+            <div className="flex flex-col md:flex-row gap-3 md:items-center">
+                <div className="flex flex-col md:flex-row gap-4 items-center border rounded-md py-2 px-3">
+                    <Link href="/about" className="hover:underline underline-offset-3 underline-width-2">About Paul</Link>
+                </div>
+                <button onClick={checkJob} className="bg-violet-400 text-white px-4 py-2 rounded-md cursor-pointer">{buttonText}</button>
+            </div>
         </header>
     );
 
