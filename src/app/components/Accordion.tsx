ae2db20dc4328bb315ac47414c674a09d4ee08e4
GordonBennett499 <gordonbennett1841@gmail.com>
2025-06-25 23:21:04 +0100

    feat: move homepage components into accordion

---
+++ src/app/components/Accordion.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { ChevronDown } from "lucide-react";
+import { useState } from "react";
+
+type AccordionProps = {
+    title: string;
+    description?: string;
+    children: React.ReactNode;
+}
+
+export default function Accordion({ title, description, children, ...params }: AccordionProps) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className="border rounded-md p-4 pt-3">
+            <div className="cursor-pointer flex flex-row justify-between items-center" onClick={() => setIsOpen(!isOpen)}>
+                <div>
+                    <h3 className="text-xl font-bold mb-0">{title}</h3>
+                    {description && <p className="text-sm mt-1">{description}</p>}
+                </div>
+                <ChevronDown size="32" className={`transition-transform duration-200 ${isOpen ? "rotate-180" : ""}`} />
+            </div>
+            <div className={isOpen ? "block mt-3" : "hidden"}>{children}</div>
+        </div>
+    );
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,23 +6,12 @@ import Card from "./components/Card";
 import Progress from "./components/Progress";
 import PromisesTable from './components/PromisesTable';
 import projects from './projects.json';
+import Accordion from './components/Accordion';
 
 export default async function Home() {
 
   return (
     <div className="">
-      <div className="mb-10">
-        <h2 className="text-xl font-bold mt-4 mb-2">Paul's Progress</h2>
-        <p>Paul is working tirelessly to complete his course "The Complete Web Developer Course 3.0" on udemy.</p>
-        <Progress title="Sections complete" x={1} y={16} />
-        <Progress title="Lectures complete" x={34} y={223} />
-        <span className="text-sm text-right text-slate-600">*This is approx. we try to keep it as up to date as possible.</span>
-        <p className="mt-3 mb-2">He has also recently started using <Link href="https://www.freecodecamp.org" target="_blank" className="text-violet-400 hover:text-violet-500">Free Code Camp</Link>. It looks like he's picked up the '<Link href="https://www.freecodecamp.org/learn/2022/responsive-web-design/" target="_blank" className="text-violet-400 hover:text-violet-500">Responsive web design</Link>' track.</p>
-        <p className="mb-2">Breaking down that course - there are 1042 individual 'steps'. I've included everything here, so these steps could be as simple as the "Hello world" start task, all the way to creating a personal portfolio site.</p>
-        <p>So far, Paul has gotten as <strong><em>opening</em></strong> the course.</p>
-        <Progress title="Free Code Camp steps complete" x={0} y={1042} />
-        <Progress title="Sections complete" x={0} y={20} />
-      </div>
       <div className="mb-10">
         <h2 className="text-xl font-bold mt-4 mb-2">Paul's Projects</h2>
         <p>Paul has a lot of projects in the works. It's doubtful any of them will ever see the light of day.</p>
@@ -40,10 +29,22 @@ export default async function Home() {
           </Link>
         </p>
       </div>
+      <div className="mb-5">
+        <Accordion title="Paul's Progress" description='Paul is working tirelessly to complete his course "The Complete Web Developer Course 3.0" on udemy.'>
+          <Progress title="Sections complete" x={1} y={16} />
+          <Progress title="Lectures complete" x={34} y={223} />
+          <span className="text-sm text-right text-slate-600">*This is approx. we try to keep it as up to date as possible.</span>
+          <p className="mt-3 mb-2">He has also recently started using <Link href="https://www.freecodecamp.org" target="_blank" className="text-violet-400 hover:text-violet-500">Free Code Camp</Link>. It looks like he's picked up the '<Link href="https://www.freecodecamp.org/learn/2022/responsive-web-design/" target="_blank" className="text-violet-400 hover:text-violet-500">Responsive web design</Link>' track.</p>
+          <p className="mb-2">Breaking down that course - there are 1042 individual 'steps'. I've included everything here, so these steps could be as simple as the "Hello world" start task, all the way to creating a personal portfolio site.</p>
+          <p>So far, Paul has gotten as <strong><em>opening</em></strong> the course.</p>
+          <Progress title="Free Code Camp steps complete" x={0} y={1042} />
+          <Progress title="Sections complete" x={0} y={20} />
+        </Accordion>
+      </div>
       <div className="mb-10">
-        <h2 className="text-lg font-bold mt-4 mb-2">Paul's Promises</h2>
-        <p>Paul likes to make promises. His apps will be live "soon"</p>
-        <PromisesTable />
+        <Accordion title="Paul's Promises" description='Paul likes to make promises. His apps will be live "soon"'>
+          <PromisesTable />
+        </Accordion>
       </div>
     </div>
   );
