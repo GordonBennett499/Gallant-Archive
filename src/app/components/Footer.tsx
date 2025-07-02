987b52183d755c59a7cf61d03ac6efa39f21abf4
GordonBennett499 <gordonbennett1841@gmail.com>
2025-07-03 00:13:41 +0100

    feat: add about the archive page

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,13 +1,19 @@
 import quotes from '@/app/quotes.json';
+import Link from 'next/link';
 
 const Footer = () => {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const randomQuote = quotes[randomIndex];
 
     return (
-        <footer className='relative mx-auto w-full max-w-6xl px-10 py-5 justify-self-end'>
-            <span className='text-2xl font-bold block mb-2'>Here's the thing</span>
-            <p className='italic text-sm text-gray-500 max-w-md'> "{randomQuote.quote}" - Paul</p>
+        <footer>
+            <div className='mx-auto w-full max-w-6xl px-10 py-5 flex flex-col-reverse md:flex-row md:justify-between gap-5 md:gap-none'>
+                <div className="relative justify-self-end">
+                    <span className='text-2xl font-bold block mb-2'>Here's the thing</span>
+                    <p className='italic text-sm text-gray-500 max-w-md'> "{randomQuote.quote}" - Paul</p>
+                </div>
+                <Link className="px-4 py-2 border rounded-md self-start md:self-end" href="/about-the-archive">About the archive</Link>
+            </div>
         </footer>
     );
 };
