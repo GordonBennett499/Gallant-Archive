32bc443fc4865460984db591d1fdf8ae15e7acf8
GordonBennett499 <gordonbennett1841@gmail.com>
2026-02-06 01:20:32 +0000

    feat: add new quotes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,16 +1,30 @@
+"use client";
+
 import quotes from '@/app/quotes.json';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const Footer = () => {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const randomQuote = quotes[randomIndex];
+
+    var [randomQuote, setRandomQuote] = useState('');
+
+    setTimeout(() => updateQuote(), 10000);
+
+    useEffect(() => {
+        updateQuote();
+    }, []);
+
+    function updateQuote() {
+        const newRandomIndex = Math.floor(Math.random() * quotes.length);
+        setRandomQuote(quotes[newRandomIndex]);
+    }
 
     return (
         <footer>
             <div className='mx-auto w-full max-w-6xl px-10 py-5 flex flex-col-reverse md:flex-row md:justify-between gap-5 md:gap-none'>
                 <div className="relative justify-self-end">
                     <span className='text-2xl font-bold block mb-2'>Here's the thing</span>
-                    <p className='italic text-sm text-gray-500 max-w-md'> "{randomQuote.quote}" - Paul</p>
+                    <p className='italic text-sm text-gray-500 max-w-md'> "{randomQuote}" - Paul</p>
                 </div>
                 <Link className="px-4 py-2 border rounded-md self-start md:self-end" href="/about-the-archive">About the archive</Link>
             </div>
